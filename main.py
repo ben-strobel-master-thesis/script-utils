91faa7e4228527146fa032e809c1128caec4eccd
Ben Strobel <benedikt.strobel@tum.de>
2024-10-22 23:03:58 +0200

    Now also able to handle actual MultiLineStrings (instead of only linestrings, that are in format of MultiLineStrings)

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import wkt_file_utils
 
 # emercast_simulator_scenario_utils.generate_crossing_scenario("C:\\Users\\bened\\Desktop\\default.emerscenario")
 # one_simulator_utils.convert_one_simulator_output_scenario("C:\\Users\\bened\\Desktop\\munich_scenario_LocationSnapshotReport.txt", "C:\\Users\\bened\\Desktop\\test.emerscenario")
-wkt_file_utils.remove_unreachable_multilines("C:\\Users\\bened\\Desktop\\munich_roads_squares_walkways.wkt", "C:\\Users\\bened\\Desktop\\processed_walkable_lines.wkt", 1)
+wkt_file_utils.remove_unreachable_multilines("C:\\Users\\bened\\Desktop\\intermediate_walkable_lines.wkt", "C:\\Users\\bened\\Desktop\\processed_walkable_lines.wkt", 1)
--- wkt_file_utils.py
+++ wkt_file_utils.py
@@ -1,22 +1,30 @@
 def remove_unreachable_multilines(input_file_path, output_file_path, root_multi_line_string_index = 1):
     with open(input_file_path, "r") as f:
         read_data = f.readlines()
-    vertices_groups = list()
+    vertices_sets = list()
+    vertices_lists = list()
     connected_vertices = set()
     connected_multi_line_indexes = set()
 
     # Parsing wkt file
-    for i in range(len(read_data)):
-        line = read_data[i]
-        line = line.replace("MultiLineString ((", "").replace("))", "")
-        segments = line.split(", ")
-        vertices = set()
-        for segment in segments:
-            coords = segment.split(" ")
-            vertices.add((coords[0], coords[1]))
-            if i == root_multi_line_string_index:
-                connected_vertices.add((coords[0], coords[1]))
-        vertices_groups.append(vertices)
+    i = 0
+    for fileline in read_data:
+        fileline = fileline.upper().replace("MULTILINESTRING", "").replace("LINESTRING", "").strip()
+        linestrings = fileline.split("),(")
+        for linestring in linestrings:
+            linestring = linestring.replace("(", "").replace(")", "").strip()
+            segments = linestring.split(", ")
+            vertices_set = set()
+            vertices_list = list()
+            for segment in segments:
+                coords = segment.split(" ")
+                vertices_set.add((coords[0], coords[1]))
+                vertices_list.append((coords[0], coords[1]))
+                if i == root_multi_line_string_index:
+                    connected_vertices.add((coords[0], coords[1]))
+            vertices_sets.append(vertices_set)
+            vertices_lists.append(vertices_list)
+            i += 1
 
     # Expanding known connected graph (contains first multi line as root), until an expansion iteration doesn't yield any new vertices
     found_new_in_last_iteration = True
@@ -25,8 +33,8 @@ def remove_unreachable_multilines(input_file_path, output_file_path, root_multi_
         iteration_number += 1
         found_new_in_last_iteration = False
         print(f"Iteration {iteration_number} Connected Vertices: {len(connected_vertices)}")
-        for i in range(len(vertices_groups)):
-            group = vertices_groups[i]
+        for i in range(len(vertices_sets)):
+            group = vertices_sets[i]
             intersection = group.intersection(connected_vertices)
             if len(intersection) > 0:
                 connected_vertices_len_before_add = len(connected_vertices)
@@ -36,11 +44,11 @@ def remove_unreachable_multilines(input_file_path, output_file_path, root_multi_
                     found_new_in_last_iteration = True
                     connected_multi_line_indexes.add(i)
 
-    print(f"Total vertices: {sum([len(x) for x in vertices_groups])} connected vertices: {len(connected_vertices)}")
-    print(f"Total multi lines: {len(read_data)} connected multi lines: {len(connected_multi_line_indexes)}")
+    print(f"Total vertices: {sum([len(x) for x in vertices_sets])} connected vertices: {len(connected_vertices)}")
+    print(f"Total linestrings: {len(read_data)} connected linestrings: {len(connected_multi_line_indexes)}")
 
     # Writing filtered wkt file
     with open(output_file_path, "w") as f:
-        for i in range(len(read_data)):
+        for i in range(len(vertices_lists)):
             if i in connected_multi_line_indexes:
-                f.write(read_data[i])
+                f.write(f"LINESTRING ({', '.join([t[0] + ' ' + t[1] for t in vertices_lists[i]])})\n\n")
